5b479e76e6a85f6d91f46be6070e1b8e99308958
Papatiara <shirazipatricia@gmail.com>
2020-09-08 10:01:37 -0700

    implemented input values

--- index.js
+++ index.js
@@ -1,150 +1,185 @@
-const getElement = document.querySelector(".circle")
-const getCircle2 = document.querySelector(".circle2")
 
-const getInputX2 = document.getElementById("X2")
-const getInputY2 = document.getElementById("Y2")
-const getInputX1 = document.getElementById("X1")
-const getInputY1 = document.getElementById("Y1")
-
-const getLine = document.getElementById("line")
-const getLineInput = document.getElementById("lineInput")
-
-const getField = document.getElementById("field")
+document.addEventListener("DOMContentLoaded", () => {
 
 
-//line width function
-let lineSize = (x1, x2, y1, y2) => {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
-}
+    const getElement = document.querySelector(".circle")
+    const getCircle2 = document.querySelector(".circle2")
 
 
-const formulaTan = (x1, x2, y1, y2) => {
-    let slope = (y2 - y1) / (x2 - x1)
-    let radians = Math.atan(slope)
-    if (y1 && y2 === 0) {
-        return 0;
-    }
-    return x1 >= x2 ? radians + Math.PI : radians;
-}
+    const getLine = document.getElementById("line")
 
-let beingDragged = ''
+    const getLineInput = document.getElementById("lineInput")
 
+    let y1 = getElement.style.top === "" ? 0 : parseInt(getElement.style.top.split("p")[0])
+    let y2 = getCircle2.style.top === "" ? 400 : parseInt(getCircle2.style.top.split("p")[0])
+    let x1 = getElement.style.left === "" ? 0 : parseInt(getElement.style.left.split("p")[0])
+    let x2 = getCircle2.style.left === "" ? 400 : parseInt(getCircle2.style.left.split("p")[0])
 
-const event1 = document.addEventListener("dragover", (e) => {
+// slope function
+    const formulaTan = (x, y) => {
+        let slope = y / x;
+        let radians = Math.atan(slope)
 
-let y1 = getElement.style.top === "" ? 0 : parseInt(getElement.style.top.split("p")[0])
-let y2 = getCircle2.style.top === "" ? 400 : parseInt(getCircle2.style.top.split("p")[0])
-let x1 = getElement.style.left === "" ? 0 : parseInt(getElement.style.left.split("p")[0])
-let x2 = getCircle2.style.left === "" ? 400 : parseInt(getCircle2.style.left.split("p")[0])
-
-let width = getLine.style.width
-
-    if (beingDragged.classList[0] === "circle") {
-        getLine.style.transform = `rotate(${formulaTan(x1, x2, y1, y2)}rad)`
-
-        console.log(x1,y1,x2,y2)
-
-        getLine.style.left = `${e.clientX + 75}px`
-        getLine.style.top = `${e.clientY + 75}px`
-        getLine.style.width = `${lineSize(y1, y2, x1, x2)}px`
-
-        getInputX1.setAttribute('value', e.clientX)
-        getInputY1.setAttribute('value', e.clientY)
-        getLineInput.setAttribute('value', width.split('p')[0].slice(0, width.indexOf('.') ))
-
-        beingDragged.style.top = `${e.clientY}px`
-        beingDragged.style.left = `${e.clientX}px`
-    } else {
-
-        getLine.style.width = `${lineSize(x1, x2, y1, y2)}px`
-        getLine.style.transform = `rotate(${formulaTan(x1, x2, y1, y2)}rad)`
-        getLineInput.setAttribute('value', width.split('p')[0].slice(0, width.indexOf('.') ))
-        getInputX2.setAttribute('value', e.clientX)
-        getInputY2.setAttribute('value', e.clientY)
-
-        beingDragged.style.top = `${e.clientY}px`
-        beingDragged.style.left = `${e.clientX}px`
-    }
-
-});
-
-
-  
-let inputsFields = document.querySelectorAll("input");
-let y1 = getElement.style.top === "" ? 0 : parseInt(getElement.style.top.split("p")[0])
-let y2 = getCircle2.style.top === "" ? 400 : parseInt(getCircle2.style.top.split("p")[0])
-let x1 = getElement.style.left === "" ? 0 : parseInt(getElement.style.left.split("p")[0])
-let x2 = getCircle2.style.left === "" ? 400 : parseInt(getCircle2.style.left.split("p")[0])
-
-
-inputsFields.forEach((el) => {
-    el.addEventListener("keyup", (event) => {
-        if (event.key === "Backspace" || typeof Number(event.key) !== "number") {
-            return;
+        if (x === 0) {
+            if (y < 0) {
+                return Math.atan(-90)
+            }
+            return Math.atan(90);
+        }
+        else if (x < 0) {
+            return radians + Math.PI
         }
 
-        if (event.target.id === "X2" || event.target.id === "Y2") {
-            if (event.target.id === "X2") {
-                x2 = Number(event.target.value);
-            } else if (event.target.id === "Y2") {
-                y2 = Number(event.target.value);
-            }
-            getLine.style.width = `${lineSize(x1, x2, y1, y2)}px`
-            getLine.style.transform = `rotate(${formulaTan(x1, x2, y1, y2)}rad)`
+        return radians;
+    }
 
-            getLineInput.setAttribute('value', getLine.style.width.split('p')[0])
-            getInputX2.setAttribute('value', x2)
-            getInputY2.setAttribute('value', y2)
-            getCircle2.style.top = `${y2 + 75}px`
-            getCircle2.style.left = `${x2 + 75}px
-            `
-        } if (event.target.id === "X1" || event.target.id === "Y1") {
-            if (event.target.id === "X1") {
-                x1 = Number(event.target.value);
-            } if (event.target.id === "Y1") {
-                y1 = Number(event.target.value);
-            }
-            getLine.style.width = `${lineSize(x1, x2, y1, y2)}px`
-            getLine.style.transform = `rotate(${formulaTan(x1, x2, y1, y2)}rad)`
+// line function 
+    const lineSize = (x, y) => {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+    }
+
+// left circle left margin
+    const circle2Left = (x, y) => {
+        let width = Number(getLine.style.width.split('p').slice(0, getLine.style.width.indexOf('.')));
+        console.log(width)
+        let rad = formulaTan(x, y)
+
+        c = width;
+        b = Math.sin(rad) * c
+        a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2))
+
+        return a;
+    }
+
+// left circle top margin
+
+    const circle2Top = (x, y) => {
+        let width = Number(getLine.style.width.split('p').slice(0, getLine.style.width.indexOf('.')));
+        let rad = formulaTan(x, y)
+
+        c = width
+        b = Math.sin(rad) * c
+        a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2))
+        return b;
+    }
+
+    // Dragover eventListener
+
+    let beingDragged = ''
+    const event1 = document.addEventListener("dragover", (e) => {
+        e.preventDefault();
+
+        let inputValue = beingDragged.querySelectorAll('input');
+
+        let x = beingDragged.classList[0] === "circle" ? x2 - e.clientX : e.clientX - x1;
+        let y = beingDragged.classList[0] === "circle" ? y2 - e.clientY : e.clientY - y1;
+
+        let width = getLine.style.width;
+
+        getLine.style.width = `${lineSize(x, y)}px`
+        getLine.style.transform = `rotate(${formulaTan(x, y)}rad)`
+        getLineInput.setAttribute('value', width.split('p')[0].slice(0, width.indexOf('.')))
+
+        inputValue[0].setAttribute('value', e.clientX);
+        inputValue[1].setAttribute('value', e.clientY);
+
+        beingDragged.style.top = `${e.clientY}px`
+        beingDragged.style.left = `${e.clientX}px`
 
 
-            getLine.style.left = `${x1 + 75}px`
-            getLine.style.top = `${y1 + 75}px`
 
-            getInputX1.setAttribute('value', x1)
-            getInputY1.setAttribute('value', y1)
-            getLineInput.setAttribute('value', getLine.style.width.split('p')[0])
-
-            getElement.style.top = `${y1}px`
-            getElement.style.left = `${x1}px`
+        if (beingDragged.classList[0] === "circle") {
+            getLine.style.left = `${e.clientX + 75}px`
+            getLine.style.top = `${e.clientY + 75}px`
+            x1 = e.clientX
+            y1 = e.clientY
         } else {
-            getLine.style.width = `${Number(event.target.value)}px`
-            let width = Number(event.target.value)
-            let rad = formulaTan(x1, x2, y1, y2)
-
-            c = width // hypoteneus
-            b = Math.sin(rad) * c // y value
-            a = Math.sqrt(Math.pow(c,2) - Math.pow(b,2)) // x value    
-            
-            
-            getCircle2.style.top = `${a}px` 
-            getCircle2.style.left = `${b}px`      
+            x2 = e.clientX
+            y2 = e.clientY
         }
+
+    }, false);
+
+
+
+    // InputFields eventListener
+
+
+    let inputsFields = document.querySelectorAll("input");
+    inputsFields.forEach((el) => {
+        el.addEventListener("keyup", (event) => {
+            if (event.target.value === 0 && event.key === "Backspace" || typeof Number(event.key) !== "number") {
+                return 0;
+            }
+
+            let inputParent = event.target.offsetParent;
+            let width = getLine.style.width;
+            let x = x2 - x1;
+            let y = y2 - y1;
+
+
+            if (event.target.id !== "lineInput") {
+                if (event.target.id === "X2" || event.target.id === "X1") {
+                    if (event.target.id === "X1") {
+                        x1 = Number(event.target.value)
+                        x = x2 - x1;
+                        inputParent.style.left = `${Number(event.target.value)}px`
+                        getLine.style.left = `${Number(event.target.value) + 75}px`
+
+                    } else {
+                        x2 = Number(event.target.value)
+                        x = x2 - x1;
+                        inputParent.style.left = `${Number(event.target.value)}px`
+                    }
+                }
+                if (event.target.id === "Y2" || event.target.id === "Y1") {
+                    if (event.target.id === "Y1") {
+                        y1 = Number(event.target.value)
+                        y = y2 - y1;
+                        inputParent.style.top = `${Number(event.target.value)}px`
+                        getLine.style.top = `${Number(event.target.value) + 75}px`
+
+                    } else {
+                        y2 = Number(event.target.value)
+                        y = y2 - y1;
+                        x = x2 - x1;
+                        inputParent.style.top = `${Number(event.target.value) - 75}px`
+                    }
+                }
+                getLineInput.setAttribute('value', width.split('p')[0].slice(0, width.indexOf('.')))
+                getLine.style.width = `${lineSize(x, y)}px`
+                getLine.style.transform = `rotate(${formulaTan(x, y)}rad)`
+
+            } else {
+                let inputValue = getCircle2.querySelectorAll('input');
+                getLine.style.width = `${Number(event.target.value)}px`
+                y2 = Math.trunc(circle2Top(x, y) + y1);
+                x2 = Math.trunc(circle2Left(x, y) + x1);
+                inputValue[0].setAttribute("value", x2 );
+                inputValue[1].setAttribute("value", y2 );
+                getCircle2.style.top = `${circle2Top(x, y)}px`
+                getCircle2.style.left = `${circle2Left(x, y)}px`
+            }
+
+        });
+    })
+
+    // remove ghost image when dragging an element
+
+    getCircle2.addEventListener("dragstart", (e) => {
+        beingDragged = e.target;
+        var img = new Image();
+        img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+        e.dataTransfer.setDragImage(img, 0, 0);
     });
-})
 
-// remove ghost image when dragging an element
+    getElement.addEventListener("dragstart", (e) => {
+        beingDragged = e.target;
+        var img = new Image();
+        img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+        e.dataTransfer.setDragImage(img, 0, 0);
+    });
 
-const eventOnCircle2 = getCircle2.addEventListener("dragstart", (e) => {
-    beingDragged = e.target;
-    var img = new Image();
-    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-    e.dataTransfer.setDragImage(img, 0, 0);
-});
 
-const eventOnCircle1 = getElement.addEventListener("dragstart", (e) => {
-    beingDragged = e.target;
-    var img = new Image();
-    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-    e.dataTransfer.setDragImage(img, 0, 0);
+
 });
